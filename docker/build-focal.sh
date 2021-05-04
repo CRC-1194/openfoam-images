9b5896a5ab3c22c3c85a73befe5917143a93f512
Tomislav Maric <tomislav.maric@gmx.com>
2021-05-04 14:08:46 +0200

    build-focal.sh added

---
+++ docker/build-focal.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+sudo docker build . \
+	-t openfoam-v2012_ubuntu-focal \ # Docker tag
+	-f openfoam-v2012_ubuntu-focal \ # Dockerfile 
+	# --no-cache # Try in case something goes wrong with package updates. 
