e07ca542bda76abd77d89ec5817169d95799aa23
Tomislav Maric <tomislav.maric@gmx.com>
2021-07-02 12:14:46 +0200

    Ubuntu Focal: OpenFOAM dynamicCode patch

--- docker/build-focal.sh
+++ docker/build-focal.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
 sudo docker build . \
-	-t openfoam-v2012_ubuntu-focal \ # Docker tag
+	-t openfoam-v2012_ubuntu-focal:latest \ # Docker tag
 	-f openfoam-v2012_ubuntu-focal \ # Dockerfile 
 	# --no-cache # Try in case something goes wrong with package updates. 
