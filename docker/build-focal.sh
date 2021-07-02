bea19e161aec843634bda8d40fcd40f3382c66e6
Tobias Tolle <bt@lefou-familie.org>
2021-07-02 13:16:49 +0200

    Rearranged comments to make build script executable; added an additional 'apt update' to avoid errors during package installation.

--- docker/build-focal.sh
+++ docker/build-focal.sh
@@ -1,6 +1,9 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
+# Options used:
+#   -t : set Docker tag
+#   -f : Docker file to be used to build the image
 sudo docker build . \
-	-t openfoam-v2012_ubuntu-focal:latest \ # Docker tag
-	-f openfoam-v2012_ubuntu-focal \ # Dockerfile 
+	-t openfoam-v2012_ubuntu-focal:latest \
+	-f openfoam-v2012_ubuntu-focal \
 	# --no-cache # Try in case something goes wrong with package updates. 
