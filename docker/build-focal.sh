a6f42e045c2db8f7ba1fd1e623922c9ce4af9b4b
Tomislav Maric <tomislav.maric@gmx.com>
2022-02-11 12:54:49 +0100

    OpenFOAM-v2112 added

--- docker/build-focal.sh
+++ docker/build-focal.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
+OPENFOAM_VERSION=$1
+
+if [ -z "$OPENFOAM_VERSION" ]; 
+then 
+    echo Provide the OpenFOAM version suffix as argument: v2012, v2112, etc.
+    exit 1
+fi
+
 # Options used:
 #   -t : set Docker tag
 #   -f : Docker file to be used to build the image
 sudo docker build . \
-	-t openfoam-v2012_ubuntu-focal:latest \
-	-f openfoam-v2012_ubuntu-focal \
+	-t openfoam-"$OPENFOAM_VERSION"_ubuntu-focal:latest \
+	-f openfoam-"$OPENFOAM_VERSION"_ubuntu-focal \
 	# --no-cache # Try in case something goes wrong with package updates. 
